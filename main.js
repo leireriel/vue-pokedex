905d04d707a5ea0329d86855f5e6f218818ef123
leireriel <leirericop@gmail.com>
2019-05-28 19:43:20 +0200

    Fetch

--- main.js
+++ main.js
@@ -4,8 +4,38 @@ const app = new Vue({
     return {
       message: 'soy el estado',
       pokemon: {
-        name: 'pikachu',
-      }
+        "id": 25,
+        "name": "pikachu",
+        "image": "images/pokemons/pikachu.png",
+        "types": [
+          "electric"
+        ],
+        "abilities": [
+          "lightning-rod",
+          "static"
+        ],
+        "experience": 112,
+        "height": 4,
+        "weight": 60
+      },
+      typeColor: {
+        "grass": "#78C850",
+        "poison": "#A040A0",
+        "fire": "#F08030",
+        "flying": "#A890F0",
+        "water": "#6890F0",
+        "bug": "#A8B820",
+        "normal": "#A8A878",
+        "electric": "#F8D030"
+      },
     };
+  },
+  created() {
+    fetch('https://api.jsonbin.io/b/5ab37f77989617146bd6eb29')
+      .then(response => response.json())
+      .then(pokemons => {
+        // debugger;
+      })
   }
 });
+
